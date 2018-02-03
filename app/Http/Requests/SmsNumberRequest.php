3eadf1817a26e3e5189106a13c7ef3d0f394a97d
vagrant <vagrant@communitytaxcms>
2018-02-03 07:56:59 +0000

    Better validation in creation sms form and app name changed

--- app/Http/Requests/SmsNumberRequest.php
+++ app/Http/Requests/SmsNumberRequest.php
@@ -24,7 +24,7 @@ class SmsNumberRequest extends FormRequest
     public function rules()
     {
         return [
-            'sms_number' => 'phone:US'
+            'sms_number' => 'required|digits:10'
         ];
     }
 }
