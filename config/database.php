80282057ad8946d50d3f7edf797380cd648adcae
Anselmo Hernandez <chemo.baza@gmail.com>
2018-02-08 15:17:14 -0600

    Set MySql as a default Database engine

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'heroku_2bd5e17017be190'),
+            'username' => env('DB_USERNAME', 'b138ad6f67194e'),
+            'password' => env('DB_PASSWORD', '1b2645fc'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
