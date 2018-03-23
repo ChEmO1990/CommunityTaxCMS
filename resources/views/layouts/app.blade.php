17e3e9e85d4807b635eb9c30cc844aa661191c8e
Anselmo Hernandez <anhernandez@communitytax.com>
2018-03-23 09:36:23 -0500

    Default icon page changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="shortcut icon" href="/favicon.png.ico" type="image/x-icon">
+
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ config('app.name', 'CommunityTax') }}</title>
